44c2b1a2c99aa5cdcedd18686763943cbd4f75a4
Inna 1201 <inna.ogurcova@gmail.com>
2022-08-08 14:18:33 +0100

    updates

--- Assets/script.js
+++ Assets/script.js
@@ -55,10 +55,10 @@ fetch(apiUrl)
         console.log(data)
 
     mainCity.textContent = name
-    mainTemp.textContent = "Temperature:" + data.current.temp
-    mainWind.textContent = "Wind:" + data.current.wind_speed
-    mainHum.textContent = "Humidity:" + data.current.humidity
-    mainUV.textContent = "UV Index:" + data.current.uvi
+    mainTemp.textContent = "Temperature: " + data.current.temp + " °C";
+    mainWind.textContent = "Wind: " + data.current.wind_speed + " MPH";
+    mainHum.textContent = "Humidity: " + data.current.humidity + " %";
+    mainUV.textContent = "UV Index: " + data.current.uvi
 
     for (var i = 1; i < 6; i++) {
         console.log(data.daily[i])
@@ -74,9 +74,24 @@ fetch(apiUrl)
         pDate.textContent = day
         col.appendChild(pDate)
 
-        // var pTemp = document.createElement("p")
-        // pDate.textContent = data.daily[i].temp[1]
-        // col.appendChild(pTemp)
+        var pIcon = document.createElement("p")
+        var iconcode = data.daily[i].weather[0].icon +".png";
+        pIcon.textContent = iconcode
+        
+        // pIcon.textContent = data.daily[i].weather.icon;
+        col.appendChild(pIcon)
+
+        var pTemp = document.createElement("p")
+        pTemp.textContent = "Temp: " + data.daily[i].temp.day + " °C";
+        col.appendChild(pTemp);
+
+        var pWind = document.createElement("p")
+        pWind.textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
+        col.appendChild(pWind)
+
+        var pHum = document.createElement("p")
+        pHum.textContent = "Humidity: " + data.daily[i].humidity + " %";
+        col.appendChild(pHum)
 
         document.querySelector(".forecast").appendChild(col)
     }
@@ -85,6 +100,22 @@ fetch(apiUrl)
     });
 
 }
+
+
+// for (var i = 1; i < 6; i++) {
+//     console.log(data.daily[i])
+//     var day = moment.unix(data.daily[i].dt);
+//     // document.querySelector('.pDate').textContent = day
+//     var col = document.createElement("div");
+//     col.setAttribute("class", "col-md-2 p-3 mb-2 bg-info text-white infoBlock");
+//     var pDate = document.createElement("p");
+//     pDate.textContent = day
+//     col.appendChild(pDate)
+//     var pTemp = document.createElement("p")
+//     pTemp.textContent = data.daily[i].temp.day;
+//     col.appendChild(pTemp)
+//     document.querySelector(".forecast").appendChild(col)
+// }
 
 
 
